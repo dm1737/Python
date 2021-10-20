102206f8b8e0747fc2ac188e17f33817c042d65a
dm1737 <davidmartin1737@gmail.com>
2021-10-20 08:56:57 -0400

    updated game_board

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -3,13 +3,15 @@ game = [[0, 0, 0,],
         [0, 0, 0,]]
 
 
-def game_board():
+def game_board(player=0, row=0, col=0, just_display=False):
     print("   0  1  2")
+    if not just_display:
+        game[row][col] = player
     for count, row in enumerate(game):
         print(count, row)
 
-
 game_board()
+game_board(player=1, row=2, col=0)
 
 
     
