cd2fd7116bd694c730c2908eeda789e60b5f6e7f
dm1737 <davidmartin1737@gmail.com>
2021-10-20 12:07:37 -0400

    Mutability update

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -3,15 +3,16 @@ game = [[0, 0, 0,],
         [0, 0, 0,]]
 
 
-def game_board(player=0, row=0, col=0, just_display=False):
+def game_board(game_map, player=0, row=0, col=0, just_display=False):
     print("   0  1  2")
     if not just_display:
-        game[row][col] = player
-    for count, row in enumerate(game):
+        game_map[row][col] = player
+    for count, row in enumerate(game_map):
         print(count, row)
 
-game_board()
-game_board(player=1, row=2, col=0)
+    return game_map
+
+game = game_board(game, player=1, row=2, col=0)
 
 
     
