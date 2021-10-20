4e7172fc73fc93fab49312cef912ef3b0566a96b
dm1737 <davidmartin1737@gmail.com>
2021-10-20 18:08:01 -0400

    Horizontal winner

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -1,6 +1,6 @@
-game = [[0, 0, 0,],
+game = [[1, 1, 1,],
         [0, 0, 0,],
-        [0, 0, 0,]]
+        [2, 2, 0,]]
 
 
 def game_board(game_map, player=0, row=0, col=0, just_display=False):
@@ -17,7 +17,15 @@ def game_board(game_map, player=0, row=0, col=0, just_display=False):
     except Exception as e:
         print("Something went very wrong...", e)
 
-game = game_board(game, player=1, row=3, col=0)
+def win(current_game):
+    for row in game:
+        print(row)
+        if row.count(row[0]) == len(row) and row[0] != 0:
+            print("Winner")
+            
+win(game)
+
+# game = game_board(game, player=1, row=3, col=0)
 
 
     
