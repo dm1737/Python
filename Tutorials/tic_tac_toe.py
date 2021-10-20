22f5107a91323d944a8c978a12af84a058a88014
dm1737 <davidmartin1737@gmail.com>
2021-10-20 18:40:35 -0400

    Vertical win logic

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -1,6 +1,6 @@
-game = [[1, 1, 1,],
-        [0, 0, 0,],
-        [2, 2, 0,]]
+game = [[1, 0, 1,],
+        [1, 0, 0,],
+        [1, 2, 0,]]
 
 
 def game_board(game_map, player=0, row=0, col=0, just_display=False):
@@ -17,13 +17,22 @@ def game_board(game_map, player=0, row=0, col=0, just_display=False):
     except Exception as e:
         print("Something went very wrong...", e)
 
+for col in range(len(game)):
+    check = []
+
+    for row in game:
+        check.append(row[col])
+    
+    if check.count(check[0]) == len(check) and check[0] != 0:
+        print("Winner!")
+
 def win(current_game):
     for row in game:
         print(row)
         if row.count(row[0]) == len(row) and row[0] != 0:
             print("Winner")
             
-win(game)
+# win(game)
 
 # game = game_board(game, player=1, row=3, col=0)
 
