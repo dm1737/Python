1a32837acd33b7e5e931f79fbabd647d2aa09254
dm1737 <davidmartin1737@gmail.com>
2021-10-20 17:49:30 -0400

    Error handling

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -4,15 +4,20 @@ game = [[0, 0, 0,],
 
 
 def game_board(game_map, player=0, row=0, col=0, just_display=False):
-    print("   0  1  2")
-    if not just_display:
-        game_map[row][col] = player
-    for count, row in enumerate(game_map):
-        print(count, row)
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][col] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Error: make sure you input row/column as 0, 1, or 2.", e)
+    
+    except Exception as e:
+        print("Something went very wrong...", e)
 
-    return game_map
-
-game = game_board(game, player=1, row=2, col=0)
+game = game_board(game, player=1, row=3, col=0)
 
 
     
