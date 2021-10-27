451c1648dd11768be52019fe81e04e9ba862fc01
dm1737 <davidmartin1737@gmail.com>
2021-10-27 08:52:44 -0400

    added colorama

--- Tutorials/tic_tac_toe.py
+++ Tutorials/tic_tac_toe.py
@@ -1,4 +1,6 @@
 import itertools
+from colorama import Fore, Back, Style, init
+init()
 
 
 def game_board(game_map, player=0, row=0, col=0, just_display=False):
@@ -9,8 +11,18 @@ def game_board(game_map, player=0, row=0, col=0, just_display=False):
         print("   "+"  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][col] = player
+
         for count, row in enumerate(game_map):
-            print(count, row)
+            colored_row = ""
+            for item in row:
+                if item == 0:
+                    colored_row += "   "
+                elif item == 1:
+                    colored_row += Fore.GREEN + ' X ' + Style.RESET_ALL
+                elif item == 2:
+                    colored_row += Fore.MAGENTA + ' O ' + Style.RESET_ALL
+            print(count, colored_row)
+
         return game_map, True
     except IndexError as e:
         print("Error: make sure you input row/column as 0, 1, or 2.", e)
